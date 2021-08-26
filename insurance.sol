096927c4d5f98b47a8681d798c1df987f14706c9
Psycho-not0078 <snbhat0708@gmail.com>
2021-08-26 09:24:39 -0700

    again init

---
+++ insurance.sol
@@ -0,0 +1,33 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Insurance{
+    struct User {
+        uint id;
+        string name;
+        ufixed claimable;
+        int role;
+    }
+    
+    struct claim {
+        uint claimId;
+        string claimStatus;
+    }
+    
+    struct renew{
+        uint renewId;
+        ufixed amount;
+        bool approvalStatus;
+    }
+    uint public userCount;
+    uint public claimCount;
+    uint public renewCount;
+    
+    constructor () public {
+        
+    }
+    
+    function addUser() public view returns(string memory){
+        
+    }
+    
+}
