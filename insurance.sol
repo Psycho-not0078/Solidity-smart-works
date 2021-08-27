d5ebf6485c8f9809c82e0f50642bf05bb980e62c
Psycho-not0078 <snbhat0708@gmail.com>
2021-08-27 08:34:41 -0700

    added count function and some more works

--- insurance.sol
+++ insurance.sol
@@ -8,7 +8,7 @@ contract Insurance{
         uint claimable;
         int role;
     }
-    mapping(uint => User) public user;// mapping a user to a integer 
+    mapping(uint => User) private user;// mapping a user to a integer 
     mapping(address => User) private userAddress;//mapping a user to his/her address to check if a incoming request is from which role user
     
     struct Claim {
@@ -17,18 +17,18 @@ contract Insurance{
         uint claimAmount;
         string claimStatus;
     }
-    mapping(uint => Claim) public claim;//mapping a claim to a integer
+    mapping(uint => Claim) private claim;//mapping a claim to a integer
     
     struct Renew{
         uint renewId;
         address renewer;
         uint amount;
     }
-    mapping(uint => Renew) public renew;//mapping a renew request to a integer
+    mapping(uint => Renew) private renew;//mapping a renew request to a integer
     
-    uint public userCount;//counter
-    uint public claimCount;//counter
-    uint public renewCount;//counter
+    uint private userCount;//counter
+    uint private claimCount;//counter
+    uint private renewCount;//counter
     
     constructor() public {
         init(msg.sender);
@@ -98,11 +98,33 @@ contract Insurance{
         return ret(_claimid,Claimer,Amt,status);
     }
     
-    function renewal(uint _amount) public{
+    function renewal(uint _amount) public{//the claim amt renew function
         require(userAddress[msg.sender].role==3);
         renewCount++;
         renew[renewCount]=Renew(renewCount,msg.sender,_amount);
         userAddress[msg.sender].claimable+=_amount;
         user[userAddress[msg.sender].id].claimable+=_amount;
+    }
+    
+    function viewclaimableamt() public view returns(uint amt){//function to view how much can be claimed
+        require(userAddress[msg.sender].role==3);
+        return userAddress[msg.sender].claimable;
+    }
+    function viewcount(int opt) public view returns(string memory, uint count){//function to do the counting according to the parameter, 1=>number of users, 2=>number of claims, 3=>number of renewals
+        string memory _rett="";
+        uint _count=0;
+        if (opt==1){
+            _rett="User Count";
+            _count=userCount;
+        }
+        else if (opt==2){
+            _rett="Claim Count";
+            _count=claimCount;
+        }
+        else if (opt==3){
+            _rett="Renew Count";
+            _count=renewCount;
+        }
+        return (_rett,_count);
     }
 }
